d015f84b3f2f37ee9bc384b22d6b380da2895555
ShayanGiri <sawon498@gmail.com>
2024-05-06 01:26:30 +0530

    adding css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Netflix from "./components/Netflix"
 
 function App() {
   return (
-      <div className="p-[30px] flex flex-wrap gap-10">
+      <div className="p-[30px] grid grid-cols-4 max-[500px]:grid-cols-1 max-[800px]:grid-cols-2 max-[1050px]:grid-cols-3 gap-10 justify-center ">
         {netflixData.map((v, i) => {
           return (
             <Netflix nitems={v} key={i} />
